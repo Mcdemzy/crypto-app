650ff9789b85fb186d20dd1879958d454482da0a
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-21 02:36:26 +0100

    Deploying to Vercel

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-// import Balance from "./component/balance/Balance.tsx";
-// import LandingPage from "./component/landingpage/LandingPage.tsx";
-
-// import Banner from "./component/banner/Banner.tsx";
 import Signin from "./component/signin/Signin.tsx";
-// import Signup from "./component/signup/Signup.tsx";
 
 function App() {
   return (
     <div className="">
-      {/* <LandingPage />
-      <Banner /> */}
       <Signin />
-      {/* <Signup /> */}
     </div>
   );
 }
