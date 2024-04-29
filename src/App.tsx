cca092289ab031078f82529f25a8d2d6e63f8dd5
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-29 12:15:31 +0100

    Designing Coming soon Page Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Coming from "./component/coming/Coming";
-import Dashboard from "./component/dashboard/Dashboard";
+// import Dashboard from "./component/dashboard/Dashboard";
 import Navbar from "./component/navbar/Navbar";
 
 function App() {
