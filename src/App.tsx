2cdfd025ab37ecc7121e43270f632475bea74c0a
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-05-03 10:52:53 +0100

    Designing Wallet Page Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,30 @@
-import { Routes, Route } from "react-router-dom";
-import Create from "./component/create/Create";
-import Dashboard from "./component/dashboard/Dashboard";
-import LandingPage from "./component/landingpage/LandingPage";
-import Signup from "./component/signup/Signup";
-import Signin from "./component/signin/Signin";
-import Banner from "./component/banner/Banner";
-import Navbar from "./component/navbar/Navbar";
+// import { Routes, Route } from "react-router-dom";
+// import Create from "./component/create/Create";
+// import Dashboard from "./component/dashboard/Dashboard";
+// import LandingPage from "./component/landingpage/LandingPage";
+// import Signup from "./component/signup/Signup";
+// import Signin from "./component/signin/Signin";
+// import Banner from "./component/banner/Banner";
+// import Navbar from "./component/navbar/Navbar";
+// import Coming from "./component/coming/Coming";
 import Wallet from "./wallet/Wallet";
-import Coming from "./component/coming/Coming";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/signin" element={<Signin />} />
-      <Route path="/create" element={<Create />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/banner" element={<Banner />} />
-      <Route path="/navbar" element={<Navbar />} />
-      <Route path="/wallet" element={<Wallet />} />
-      <Route path="/coming" element={<Coming />} />
-      {/* Add more routes for other components if needed */}
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<LandingPage />} />
+    //   <Route path="/signup" element={<Signup />} />
+    //   <Route path="/signin" element={<Signin />} />
+    //   <Route path="/create" element={<Create />} />
+    //   <Route path="/dashboard" element={<Dashboard />} />
+    //   <Route path="/banner" element={<Banner />} />
+    //   <Route path="/navbar" element={<Navbar />} />
+    //   <Route path="/wallet" element={<Wallet />} />
+    //   <Route path="/coming" element={<Coming />} />
+    //   {/* Add more routes for other components if needed */}
+    // </Routes>
+    // <Wallet />
+    <Wallet />
   );
 }
 
--- src/wallet/Wallet.tsx
+++ src/wallet/Wallet.tsx
@@ -1,7 +1,279 @@
+import { BiBell } from "react-icons/bi";
+import {
+  BsBoxArrowInDownLeft,
+  BsBoxArrowUpRight,
+  BsThreeDotsVertical,
+} from "react-icons/bs";
+import Flag from "../assets/flag.png";
+import "./wallet.css";
+import { IoIosArrowDown } from "react-icons/io";
+import { BiHide } from "react-icons/bi";
+import { IoMdStopwatch } from "react-icons/io";
+
+import CryptoIcon1 from "../assets/crypto1.png";
+import CryptoIcon2 from "../assets/crypto2.png";
+import CryptoIcon3 from "../assets/crypto3.png";
+import CryptoIcon4 from "../assets/crypto4.png";
+import CryptoIcon5 from "../assets/crypto5.png";
+import CryptoIcon6 from "../assets/crypto6.png";
+import CryptoIcon7 from "../assets/crypto7.png";
+
 const Wallet = () => {
   return (
     <>
-      <section className="wallet__container">Wallet</section>
+      <section className="wallet__component">
+        <div className="flex justify-between pt-[16px] p-[10px] bg-[#121A25]">
+          <h1 className="text-[1.7rem] font-[700] text-white">Wallet </h1>
+
+          <div className="flex  mt-[10px] gap-2 justify-center">
+            <a href="">
+              <BiBell className="bg-[#222A37] p-[6px] text-[2.5rem] rounded-full text-white" />
+            </a>
+            <a href="">
+              <BsThreeDotsVertical className="text-[2rem] text-white" />
+            </a>
+          </div>
+        </div>
+
+        <div className="wallet__flag">
+          <img src={Flag} alt="" width={48} />
+          <span>NGN</span>
+          <IoIosArrowDown className="wallet__icon" />
+        </div>
+
+        <div className="">
+          <h3 className="choose__wallet">Choose Wallet</h3>
+          <div className="wallet__container">
+            <p>Crypto</p>
+            <p>Fiat</p>
+          </div>
+        </div>
+
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon1} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon2} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon3} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon4} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon5} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon6} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+        <>
+          {" "}
+          <div className="wallet__details-container">
+            <div className="wallet__details">
+              <img src={CryptoIcon7} alt="" />
+              <div>
+                <p className="text-[#707579] text-[1rem]">
+                  Bitcoin Cash Wallet
+                </p>
+                <p className="flex items-center gap-4 text-[1.3rem] ">
+                  BNB *.** <BiHide />
+                </p>
+                <p className="text-[#707579] text-[14px] mt-3">
+                  ~ ₦ 0.00 ~ $ 0.00
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-center items-center text-[1.5rem]">
+              <IoMdStopwatch />
+            </div>
+            <div className="wallet__links">
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowUpRight className="wallet__links-icon" />
+                <span>Send</span>
+              </div>
+              <div className="flex flex-col justify-center items-center">
+                <BsBoxArrowInDownLeft className="wallet__links-icon" />
+                <span>Receive</span>
+              </div>
+            </div>
+          </div>
+        </>
+      </section>
     </>
   );
 };
